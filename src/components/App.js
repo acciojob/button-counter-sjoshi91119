8f734617a7b0ff235e4151d7780eae7c751b9d36
Shubham Joshi <sjoshi91119@gmail.com>
2023-06-28 21:38:28 +0530

    submitting code-Wed Jun 28 2023 21:38:27 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import { getEventListeners } from "events";
 
 const App = () => {
+
+  const [number, setnumber] = useState(0);
+
+  const handleClick =() => {
+    setnumber(number + 1);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      
+      <p>button clicked {number} times</p>
+
+      <button onClick={handleClick} >Click me</button>
+
     </div>
   )
 }
